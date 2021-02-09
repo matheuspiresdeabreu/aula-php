a8e734bb8f64783b6a529161a490bb9e0f9fb6d9
Pires <matheus.1001@hotmail.com>
2021-02-09 19:56:12 -0300

    layoyt da agenda

---
+++ agenda/index.php
@@ -0,0 +1,84 @@
+<html> 
+<head>
+<title> agenda</title>
+
+
+</head>
+
+<body>
+     
+     <h1>agenda de contatos </h1>
+
+ <form  method="POST"> 
+  <fieldset>
+  <legend>  dados do contato</legend>
+  <label> nome </label>
+  <input type="text" name = "nome">
+  <br>
+
+  <label>telefone</label>
+  <input type="text " name = "telefone">
+  <br>
+
+  <label >email</label>
+  <input type="text" name= "email">
+  <br>
+
+<label> cidade</label>
+<input type="text" name="cidade">
+<br>
+
+<button type= "submit">cadastro</button>
+</fieldset>
+  
+ </form>
+
+ <table>
+ <thead>
+ <tr>
+ <th>nome </th>
+ <th>telefone</th>
+ <th>email</th>
+ <th>cidade</th>
+ 
+ 
+ 
+ </tr>
+ 
+ 
+ 
+ </thead>
+ 
+ <tbody>
+ <tr>
+ <td>Bruce Waine</td>
+ <td>11-2222-3333</td>
+ <td>bruce@waine.com</td>
+ <td>Gothan City</td>
+ 
+ 
+ 
+ </tr>
+
+
+
+ 
+ 
+ 
+ 
+ 
+ </tbody>
+ 
+ 
+ 
+ 
+ 
+ </table>
+</body>
+
+
+
+
+
+
+</html>
